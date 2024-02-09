b645140a678079201a2ed55f476609f0d147e1ce
abdulllkhan <abdulskhan47@gmail.com>
2024-02-09 00:21:25 -0500

    Changes to capitalize the first letter of the reversed sentence

--- ass2/Assignment2/PartIII/ReverseSentence.java
+++ ass2/Assignment2/PartIII/ReverseSentence.java
@@ -3,11 +3,15 @@ public class ReverseSentence {
 
 	public static String reverseSentence(String sentence) {
 
+		sentence = sentence.substring(0, 1).toLowerCase() + sentence.substring(1);
 		String[] words = sentence.split(" ");
+
 		String reversed = "";
 		for(int i = words.length - 1; i > -1; i--){	
 			reversed += words[i] + " ";
 		}
+
+		reversed = reversed.substring(0, 1).toUpperCase() + reversed.substring(1);
 
 		return reversed.substring(0, reversed.length() - 1);
 	}
